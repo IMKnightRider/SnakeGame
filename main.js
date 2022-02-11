1742badf4ac5157344fdfb286ee0b5c6f7d780ec
IMKnightRider <95973265+IMKnightRider@users.noreply.github.com>
2022-02-11 11:43:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ button.addEventListener('click', async (e) => {
 
 function question(){
     let location = [[39.2,0],[88.2,-9.8],[49,-9.8]]
-    if((marginLeft() == location[0][0] && marginTop() == location[1][1]) || (marginLeft() == location[2][0] && marginTop() == location[3][1])){
+    if((marginLeft() == location[0][0] && marginTop() == location[0][1]) || (marginLeft() == location[1][0] && marginTop() == location[1][1]) || (marginLeft() == location[2][0] && marginTop() == location[2][1])){
         console.log("Question")
     }
 }
